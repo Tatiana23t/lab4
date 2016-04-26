c08024b48f534eb933394bac2fa907cbcc575f5d
Skorbilina Tatiana <tanusha23t@yandex.ru>
2016-04-26 21:15:31 +0300

    create vector and operation

---
+++ Vector.java
@@ -0,0 +1,44 @@
+package Vector;
+import java.math.*;
+public class Vector {
+	private double x;
+	private double y;
+	public Vector(){
+		x = 0;
+		y = 0;
+		}
+	public Vector(double x, double y){
+		this.x = x;
+		this.y = y;
+		}
+	public double getX(){
+		return x;
+		}
+	public double getY(){
+		return y;
+		}
+	public void setX(double z){
+		x = z;
+	}
+	
+	public void setY(double z){
+		this.y = z;
+	}
+	public double lenght(){
+		//return x+y;
+		return Math.sqrt(x*x + y*y);
+		}
+	public Vector multiplicate(double k){
+		return new Vector(x*k,y*k);
+		}
+	public Vector multiplicate(Vector b){
+		return new Vector(x*b.getX(),y*b.getY());
+		}
+	public Vector addition(Vector b){
+		return new Vector(x+b.getX(),y+b.getY());
+		}
+	
+	public Vector subtraction(Vector b){
+		return new Vector(x-b.getX(),y-b.getY());
+		}
+	}
