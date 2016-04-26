b38875b9af31102e75197acec52cf268cc017b9c
Solomatnikova Alesya <tim-terem@mail.ru>
2016-04-27 00:22:30 +0300

    add operation container

---
+++ OperationContainer.java
@@ -0,0 +1,23 @@
+package Vector;
+
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class OperationContainer extends JPanel{
+	
+	public OperationContainer(){
+		
+		
+	}
+/*	public JComboBox getComboBox(){
+		return operation;
+	}
+	public void setComboBox(JComboBox box){
+		operation = box;
+		this.add(operation);
+		this.repaint();
+		this.validate();
+	}*/
+}
